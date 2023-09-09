c71e252c3a3f17e1092e1af00b421daa4c256de6
Alien <alongejoshua@gmail.com>
2023-09-09 23:56:44 +0100

    setup HTML, CSS and JS

---
+++ library/script.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+function Book() {
+    constructor(title, price) {
+
+    }
+}
+
+function addBookToLibrary() {
+    // recieve the inputted details
+    // create a book instance with details
+    // add new book into the array
+}
+
+function removeBookFromLibrary() {
+    
+}
